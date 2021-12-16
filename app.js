9d9edf2b7331d8a4722512d4b48cead2a3e87b36
kakalima76 <nieraldo@gmail.com>
2021-12-16 14:59:10 -0300

    favicon

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var favicon = require('serve-favicon')
+
 var app = express();
 
 // view engine setup
@@ -18,6 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -37,5 +40,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 module.exports = app;
